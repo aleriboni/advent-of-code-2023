26848c5da891f7b1d3fb6cb95dfe0eeef0059bef
aleriboni <alessandro.riboni@agilelab.it>
2023-12-03 16:22:01 +0100

    Rename function for part two

--- day_03/main.py
+++ day_03/main.py
@@ -20,7 +20,7 @@ def retrieve_row_part_numbers(rows: List[str]) -> List[int]:
             part_numbers.append(int(match.group(0)))
     return part_numbers
 
-def retrieve_gear_ratios(rows: List[str]) -> List[int]:
+def retrieve_row_gear_ratios(rows: List[str]) -> List[int]:
 
     gear_ratios = []
     min_size = 0
@@ -33,15 +33,18 @@ def retrieve_gear_ratios(rows: List[str]) -> List[int]:
         for row in rows:
             candidates_numbers = re.finditer(r'\d+', row)
             for match_number in candidates_numbers:
-                if gear_position >= match_number.start() - 1 and gear_position <= match_number.end():
+                if gear_position >= match_number.start() - 1  \
+                    and gear_position <= match_number.end():
                     gear_found.append(int(match_number.group(0)))
         if len(gear_found) == 2:
             gear_ratios.append(gear_found[0] * gear_found[1])
     return gear_ratios
+
+
 def main():
 
     puzzle_input = load_txt('resources/puzzle_input.txt')
-    
+
     # Part one
     # add first and last line
     dummy_line = ['.' * len(puzzle_input[0])]
@@ -54,9 +57,9 @@ def main():
     print(f"Sum of part numbers: {sum_part_numbers}")
 
     # Part two
-    gear_ratios = [retrieve_gear_ratios(puzzle_input[i:i+3])
+    gear_ratios = [retrieve_row_gear_ratios(puzzle_input[i:i+3])
                    for i in range(0, len(puzzle_input) - 2)]
-    
+
     sum_gear_ratios = sum([sum(row_gear_ratios) for row_gear_ratios in gear_ratios])
     print(f"Sum of gear ratios: {sum_gear_ratios}")
 if __name__ == "__main__":
